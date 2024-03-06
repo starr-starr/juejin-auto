a7b72ed7621c57d6faf2560b58df25c7375e831e
starr-starr <2654109566@qq.com>
2024-03-06 12:21:13 +0800

    feat: add config

---
+++ config/index.js
@@ -0,0 +1,7 @@
+module.exports = {
+    url:{
+        sign_in:"https:api.juejin.cn/growth_api/v1/check_in",
+        free_count:"https://api.juejin.cn/growth_api/v1/lottery_config/get",
+        lottery:"https://api.juejin.cn/growth_api/v1/lottery/draw",
+    }
+}
