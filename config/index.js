1815ff2e8df16505126de599f391d909d7a5a9f4
starr-starr <2654109566@qq.com>
2024-03-09 16:25:50 +0800

    feat: add configuration with sensitive information

--- config/index.js
+++ config/index.js
@@ -3,5 +3,8 @@ module.exports = {
         sign_in:"https:api.juejin.cn/growth_api/v1/check_in",
         free_count:"https://api.juejin.cn/growth_api/v1/lottery_config/get",
         lottery:"https://api.juejin.cn/growth_api/v1/lottery/draw",
-    }
+        feishu_Robot:process.env.FEISHU_ROBOT,
+    },
+    cookie: process.env.COOKIE,
+    secret: process.env.SECRET,
 }
