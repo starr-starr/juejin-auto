69f7386d0198197defe7f0a078a433fd012f45fa
starr-starr <2654109566@qq.com>
2024-03-09 16:52:47 +0800

    fix: url spelling mistakes

--- config/index.js
+++ config/index.js
@@ -1,6 +1,6 @@
 module.exports = {
     url:{
-        sign_in:"https:api.juejin.cn/growth_api/v1/check_in",
+        sign_in:"https://api.juejin.cn/growth_api/v1/check_in",
         free_count:"https://api.juejin.cn/growth_api/v1/lottery_config/get",
         lottery:"https://api.juejin.cn/growth_api/v1/lottery/draw",
         feishu_Robot:process.env.FEISHU_ROBOT,
